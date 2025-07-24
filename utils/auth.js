3cbba9d3c7e9c09cca7fa01c1cd95d06c07eea19
MINKYOUNG\kyoun <vldzmehowl32@naver.com>
2025-07-24 10:36:08 +0900

    수정

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
 // utils/auth.js
 export function getToken() {
   if (typeof window !== 'undefined') {
-    return localStorage.getItem('access_token'); // ✅ access_token 으로 수정!
+    return localStorage.getItem('access_token'); 
   }
   return null;
 }
